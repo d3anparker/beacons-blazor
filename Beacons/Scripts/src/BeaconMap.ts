e79f7945276ec4b196082bedafe16b1edcbf6283
Dean Parker <llamagrinder@gmail.com>
2022-10-04 17:08:55 +0100

    Rearrange

---
+++ Beacons/Scripts/src/BeaconMap.ts
@@ -0,0 +1,20 @@
+﻿declare var L: any;
+//import * as L from 'leaflet';
+
+export class BeaconMap {
+    constructor(private mapId: string) {
+    }
+
+    public updateMap = (lat: number, lng: number) => {
+        const map = L.map(this.mapId);        
+        
+        map.setView([lat, lng], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 13,
+            attribution: '© OpenStreetMap',
+        }).addTo(map);
+
+        L.marker([lat, lng]).addTo(map);
+    }
+}
